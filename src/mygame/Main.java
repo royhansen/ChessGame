528071de971508fd06e8cba63f3f5e8a84e442d7
Roy Hansen <royandjess@yahoo.com>
2016-09-23 14:50:07 -0600

    updated comments and removed unused imports in main

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,13 +1,8 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 
-/**
- * test
- * @author normenhansen
- */
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
